72f7fa7658cbc482a839f51930a67632632202f9
abhinav <tiwariji.abhi21@gmail.com>
2021-06-22 14:51:54 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    platform=new Ground(150,305,300,170)
     bird = new Bird(100,100);
 
 }
@@ -41,6 +41,7 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+    platform.display()
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
